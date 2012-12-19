d8ff0b662cac50128dab7c720c97964c0d7c66db
Brent Hagany <brent@hiidef.com>
2012-12-19 01:15:21 -0600

    Use pk attributes instead of id directly; make the pk value you send to backbone configurable; stop doing extra db reads on PUT and POST; add a few ways to customize object serialization

--- djangbone/views.py
+++ djangbone/views.py
@@ -24,7 +24,6 @@ class DjangboneJSONEncoder(json.JSONEncoder):
             return None
 
 
-
 class BackboneAPIView(View):
     """
     Abstract class view, which makes it easy for subclasses to talk to backbone.js.
@@ -37,6 +36,11 @@ class BackboneAPIView(View):
     """
     base_queryset = None        # Queryset to use for all data accesses, eg. User.objects.all()
     serialize_fields = tuple()  # Tuple of field names that should appear in json output
+    custom_values = {}          # A dict of functions to run for populating response fields, eg. {'rel_name': lambda x: x.related.name}
+    values_override = None      # A single function to run to populate the whole response, eg. lambda x: x.backboneJSON
+
+    # Field name to use as the primary key
+    pk = 'id'
 
     # Optional pagination settings:
     page_size = None            # Set to an integer to enable GET pagination (at the specified page size)
@@ -54,7 +58,7 @@ class BackboneAPIView(View):
         """
         Handle GET requests, either for a single resource or a collection.
         """
-        if kwargs.get('id'):
+        if kwargs.get(self.pk):
             return self.get_single_item(request, *args, **kwargs)
         else:
             return self.get_collection(request, *args, **kwargs)
@@ -64,7 +68,7 @@ class BackboneAPIView(View):
         Handle a GET request for a single model instance.
         """
         try:
-            qs = self.base_queryset.filter(id=kwargs['id'])
+            qs = self.base_queryset.filter(pk=kwargs[self.pk])
             assert len(qs) == 1
         except AssertionError:
             raise Http404
@@ -100,11 +104,8 @@ class BackboneAPIView(View):
         if hasattr(form, 'set_request'):
             form.set_request(request)
         if form.is_valid():
-            new_object = form.save()
-            # Serialize the new object to json using our built-in methods.
-            # The extra DB read here is not ideal, but it keeps the code DRY:
-            wrapper_qs = self.base_queryset.filter(id=new_object.id)
-            return self.success_response(self.serialize_qs(wrapper_qs, single_object=True))
+            obj = form.save()
+            return self.success_response(self.serialize_qs([obj], single_object=True))
         else:
             return self.validation_error_response(form.errors)
 
@@ -116,12 +117,12 @@ class BackboneAPIView(View):
         by the subclass. This should be a ModelForm corresponding to the model used by
         base_queryset.
         """
-        if self.edit_form_class == None or not kwargs.has_key('id'):
+        if self.edit_form_class == None or self.pk not in kwargs:
             return HttpResponse('PUT not supported', status=405)
         try:
             # Just like with POST requests, Backbone will send the object's data as json:
             request_dict = self.json_decoder.decode(request.raw_post_data)
-            instance = self.base_queryset.get(id=kwargs['id'])
+            instance = self.base_queryset.get(pk=kwargs[self.pk])
         except ValueError:
             return HttpResponse('Invalid PUT JSON', status=400)
         except ObjectDoesNotExist:
@@ -130,9 +131,8 @@ class BackboneAPIView(View):
         if hasattr(form, 'set_request'):
             form.set_request(request)
         if form.is_valid():
-            item = form.save()
-            wrapper_qs = self.base_queryset.filter(id=item.id)
-            return self.success_response(self.serialize_qs(wrapper_qs, single_object=True))
+            obj = form.save()
+            return self.success_response(self.serialize_qs([obj], single_object=True))
         else:
             return self.validation_error_response(form.errors)
 
@@ -140,9 +140,9 @@ class BackboneAPIView(View):
         """
         Respond to DELETE requests by deleting the model and returning its JSON representation.
         """
-        if not kwargs.has_key('id'):
+        if self.pk not in kwargs:
             return HttpResponse('DELETE is not supported for collections', status=405)
-        qs = self.base_queryset.filter(id=kwargs['id'])
+        qs = self.base_queryset.filter(pk=kwargs[self.pk])
         if qs:
             output = self.serialize_qs(qs)
             qs.delete()
@@ -157,11 +157,11 @@ class BackboneAPIView(View):
         If the single_object argument is True, or the url specified an id, return a
         single JSON object, otherwise return a JSON array of objects.
         """
-        values = queryset.values(*self.serialize_fields)
-        if single_object or self.kwargs.get('id'):
+        if single_object or self.kwargs.get(self.pk):
             # For single-item requests, convert ValuesQueryset to a dict simply
             # by slicing the first item:
-            json_output = self.json_encoder.encode(values[0])
+            data = self.get_values(queryset[:1])[0]
+            json_output = self.json_encoder.encode(data)
         else:
             # Process pagination options if they are enabled:
             if isinstance(self.page_size, int):
@@ -170,9 +170,24 @@ class BackboneAPIView(View):
                     offset = (page_number - 1) * self.page_size
                 except ValueError:
                     offset = 0
-                values = values[offset:offset+self.page_size]
-            json_output = self.json_encoder.encode(list(values))
+                queryset = queryset[offset:offset + self.page_size]
+            json_output = self.json_encoder.encode(self.get_values(queryset))
         return json_output
+
+    def get_values(self, qs):
+        if self.values_override:
+            values = [self.values_override(obj) for obj in qs]
+        elif self.custom_values or not callable(getattr(qs, 'values', None)):
+            values = []
+            for obj in qs:
+                vals = dict([(name, getattr(obj, name)) for name in self.serialize_fields])
+                for name, method in self.custom_values.items():
+                    vals[name] = method(obj)
+                values.append(vals)
+        else:
+            values = list(qs.values(*self.serialize_fields))
+
+        return values
 
     def success_response(self, output):
         """
